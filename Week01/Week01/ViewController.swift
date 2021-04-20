b2172094eefb5772cb195b239df44c82497a5527
lee_jongseok01 <lee_jongseok01@eland.co.kr>
2021-04-20 19:42:02 +0900

    나머지는...공부하면서

--- Week01/Week01/ViewController.swift
+++ Week01/Week01/ViewController.swift
@@ -9,12 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnRow01: UIView!
+    @IBOutlet weak var btnRow02: UIView!
+    @IBOutlet weak var btnRow03: UIView!
+    @IBOutlet weak var btnRow04: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad();
-        
+        btnGroupArrangement();
     }
-
+    
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        btnGroupArrangement();
+    }
+    
+    
+    // 동적으로 레이아웃 변경할수 있도록...
+    // 다음에 ... ㅠㅠ
+    
+    func btnGroupArrangement() {
+        
+        /*self.btnRow01.removeConstraints(self.btnRow01.constraints);
+        self.btnRow02.removeConstraints(self.btnRow02.constraints);
+        self.btnRow03.removeConstraints(self.btnRow03.constraints);
+        self.btnRow04.removeConstraints(self.btnRow04.constraints);
+        */
+        if isLandscape() {
+                
+            self.btnRow01.isHidden = true;
+            self.btnRow02.isHidden = true;
+            self.btnRow03.isHidden = true;
+            self.btnRow04.isHidden = true;
+            
+        } else {
+            
+            self.btnRow01.isHidden = false;
+            self.btnRow02.isHidden = false;
+            self.btnRow03.isHidden = false;
+            self.btnRow04.isHidden = false;
+        }
+    }
+    
+    func isLandscape()->Bool
+    {
+        return UIDevice.current.orientation.isLandscape;
+    }
 
 }
 
